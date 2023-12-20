ff2c32ddbd78868c84f22b71f95fb262341be437
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2023-12-20 18:05:01 +0100

    Add nav button in learn screen

--- mobile-app/components/IconButton.js
+++ mobile-app/components/IconButton.js
@@ -5,8 +5,7 @@ import { Text, Pressable, View, StyleSheet } from "react-native";
  * @description Supports MaterialIcons, also see https://icons.expo.fyi/Index
  */
 export default function IconButton({
-    onPress, style, iconName = 'note-add',iconSize = 30, iconColor = '#4a4a8f',
-    left = false
+    onPress, style, iconName = 'note-add',iconSize = 30, iconColor = '#4a4a8f'
 }) {
     return (
         <Pressable style={style} onPress={onPress}>
