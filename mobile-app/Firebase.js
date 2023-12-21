e7b37a4ec7145c201ccc4976e80d456809c14ea9
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2023-12-21 20:55:33 +0100

    init Firebase config

---
+++ mobile-app/Firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+
+
+    apiKey: "AIzaSyDg8gPBTE7WIagKyrW-WYUx-uw7c_ByZgo",
+    authDomain: "rnapp-learnigcards.firebaseapp.com",
+    databaseURL: 'https://rnapp-learnigcards.firebaseio.com',
+    projectId: 'rnapp-learnigcards',
+    storageBucket: 'rnapp-learnigcards.appspot.com',
+    messagingSenderId: '438738784563',
+    appId: '1:438738784563:web:86170e5c102a42f3a8ee46',
+    measurementId: 'G-measurement-id',
+};
+
+export default class Firebase{
+    static db;
+
+    static init(){
+        const app = initializeApp(firebaseConfig);
+        this.db = getFirestore(app);
+    }
+}
